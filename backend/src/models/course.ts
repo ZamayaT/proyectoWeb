10787cca3e4c128db4c747bfe26b6bba19c993a9
Zamaya Toledo <zamy.canela@gmail.com>
2025-10-11 23:15:30 -0300

    added attribute 'required' to course

--- backend/src/models/course.ts
+++ backend/src/models/course.ts
@@ -17,12 +17,14 @@ interface ICourse extends Document {
   code: string,
   name: string,
   difficulty: number,
+  required: boolean,
 }
 
 const courseSchema = new Schema<ICourse>({
     code: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     difficulty: { type: Number, required: true },
+    required: { type: Boolean, required: true }
   }, {
     timestamps: true,
 });
