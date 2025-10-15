ce1e200fb5f9e79e21c3547fcee8a73d2d2c1a25
Cristian <cristianignacio71@gmail.com>
2025-10-15 01:26:52 -0300

    :sparkles: feat: add configuration and logging utilities

---
+++ backend/src/utils/config.ts
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT;
+const HOST = process.env.HOST || "localhost";
+
+const MONGODB_URI =
+    process.env.NODE_ENV === "test"
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
+
+const JWT_SECRET = process.env.JWT_SECRET || "my_secret";
+const MONGODB_DBNAME =
+    process.env.NODE_ENV === "test" 
+    ? process.env.TEST_MONGODB_DBNAME 
+    : process.env.MONGODB_DBNAME || "proejectdb"
+
+export default { PORT, HOST, MONGODB_URI, JWT_SECRET, MONGODB_DBNAME}
