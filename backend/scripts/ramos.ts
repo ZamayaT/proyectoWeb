0c288c501c1ef1745857834ce4bbe23b52fa5163
Cristian <cristianignacio71@gmail.com>
2025-10-20 21:14:39 -0300

    feat: add ramos data and seed script for initial database population

---
+++ backend/scripts/ramos.ts
@@ -0,0 +1,43 @@
+
+// Datos estáticos de los ramos DCC
+// export const ramos: Ramo[] = [
+export const ramos = [
+  {
+    code: "CC1002",
+    name: "Introducción a Programación",
+    difficulty: 2,
+    required : true
+  },
+  {
+    code: "CC3001",
+    name: "Algoritmos y Estructuras de Datos",
+    difficulty: 3,
+    required : true
+  },
+  {
+    code: "CC3101",
+    name: "Matemáticas Discretas para la Computación",
+    difficulty: 4,
+    required : true
+  },
+  {
+    code: "CC3301",
+    name: "Programación de Software de Sistemas",
+    difficulty: 5,
+    required : true
+  },
+  {
+    code: "CC5205",
+    name: "Minería de Datos",
+    difficulty: 2,
+    required : true
+  },
+  {
+    code: "CC4302",
+    name: "Sistemas Operativos",
+    difficulty: 6,
+    required : true
+  }
+];
+
+export default ramos
---
+++ backend/scripts/seed.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import {CourseModel} from "../src/models/course"; // ajustá según tu estructura
+import {UserModel} from "../src/models/user";
+import ramos from "./ramos";
+import bcrypt from "bcrypt"
+import config from "../src/utils/config"
+
+dotenv.config();
+
+const MONGODB_URI = config.MONGODB_URI || "mongodb://localhost:27017";
+
+const seed = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI, {dbName : config.MONGODB_DBNAME} );
+    console.log("✅ Conectado a MongoDB");
+
+    // Limpia colecciones antiguas
+    await CourseModel.deleteMany({});
+    await UserModel.deleteMany({});
+
+    // Inserta datos iniciales
+    const courses = await CourseModel.insertMany(ramos);
+
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(config.ADMIN_PASSWORD, saltRounds);
+
+    const admin = new UserModel({
+        username: config.ADMIN_USER,
+        email: config.ADMIN_EMAIL,
+        password: passwordHash,
+    });
+
+    await admin.save();
+
+    console.log("✅ Datos insertados:", courses.length, "cursos, 1 usuario root");
+    mongoose.connection.close();
+  } catch (err) {
+    console.error("❌ Error al ejecutar seed:", err);
+    process.exit(1);
+  }
+};
+
+seed();
